fc282fa370aa3e4b36649701f3103c6e77047072
Haoran-Lin <hr.edward.lin@gmail.com>
2021-04-26 17:27:37 -0400

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,12 @@ import '@firebase/auth';
 import '@firebase/database';
 
 const firebaseConfig = {
-    apiKey: "Copy Your config from the firebase console"
+    apiKey:"AIzaSyDAbEBL0-JSAob8vy71FQBs7TNGY8SlxNY",
+    authDomain: "ux308-final.firebaseapp.com",
+    projectId: "ux308-final",
+    storageBucket: "ux308-final.appspot.com",
+    messagingSenderId: "369464697812",
+    appId: "1:369464697812:web:c4e1e5a2fd48a637dfac41"
 };
 
 if (!firebase.apps.length) {
